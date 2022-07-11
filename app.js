d8ce73251606babfaf4b5e64801acd28338a978a
prayoga <prayoga2999@gmail.com>
2022-07-11 22:23:45 +0700

    finished

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const express = require("express");
-const { routeUsers } = require("./Users/userRouter");
 const app = express();
+const { routeUsers } = require("./Users/userRouter");
 const port = 8002;
 
 app.use(express.json());
 
-app.use(express.static("Public"));
-app.use(express.static(__dirname + "/Public/CH3"));
-app.use(express.static(__dirname + "/Public/CH4"));
+app.use(express.static(__dirname + "/public/CH3"));
+app.use(express.static(__dirname + "/public/CH4"));
 app.set("view engine", "html");
-
-app.set("views", "/views");
 app.engine("html", require("ejs").renderFile);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("ch3/index");
 });
 
 app.get("/game", (req, res) => {
-  res.render("index2");
+  res.render("ch4/index");
 });
 
 app.use(routeUsers);
